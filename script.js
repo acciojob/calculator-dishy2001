59c038e0e7e3edf8610837c4b17ac3c92e29ccad
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-14 22:39:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 //your code here
 function updatescreen(v) {
   let res = document.getElementById("screen").value += v;
-  if(res.length > 2) {
-    result();
-  }
+  // if(res.length > 2) {
+  //   result();
+  // }
+  result(res);
 }
 
-function result() {
-  let v = document.getElementById("screen").value.at(-1);
-  if(v.match(/\W/g) == null){
-    document.getElementById("screen").value = eval(document.getElementById("screen").value);
+function result(val) {
+  let v = val.at(-1);
+  if(let str = v.match(/\W/g) == null){
+	  let ind = val.indexOf(str);
+	  val = val.slice(0, ind);
   }
+    document.getElementById("preCalc").value = eval(val);
 }
-
+function ans() {
+	document.getElementById("screen").value = eval(document.getElementById("screen").value);
+}
 function reset() {
   document.getElementById("screen").value = '';
+}
+function back() {
+	let prev = document.getElementById("screen").value;
+	let newVal = document.getElementById("screen").value = prev.slice(0, prev.length-1);
+	result(newVal);
 }
