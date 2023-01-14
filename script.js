b41d765f56d86d7e758c7324cafd38b75f96bb70
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-14 23:23:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-//your code here
 function updatescreen(v) {
-  let res = document.getElementById("screen").value += v;
-  // if(res.length > 2) {
-  //   result();
-  // }
-  result(res);
-}
-
-function result(val) {
-  let v = val.at(-1);
-  if(let str = v.match(/\W/g) == null){
-	  let ind = val.indexOf(str);
-	  val = val.slice(0, ind);
+    let res;
+    if(document.getElementById("screen").value == "0" && v.match(/\W/g) == null){
+        res = document.getElementById("screen").value = v;
+    }
+    else{
+        res = document.getElementById("screen").value += v;
+    }
+    // if(res.length > 2) {
+    //   result();
+    // }
+    result(res);
   }
-    document.getElementById("preCalc").value = eval(val);
-}
-function ans() {
-	document.getElementById("screen").value = eval(document.getElementById("screen").value);
-}
-function reset() {
-  document.getElementById("screen").value = '';
-}
-function back() {
-	let prev = document.getElementById("screen").value;
-	let newVal = document.getElementById("screen").value = prev.slice(0, prev.length-1);
-	result(newVal);
-}
+  
+  function result(val) {
+    let v = val.at(-1);
+      let str;
+      if(val == "" || (str = v.match(/\W/g)) != null){
+        let ind = val.lastIndexOf(str);
+        if(ind >= 0)
+        val = val.slice(0, ind);
+    }
+      else{}
+      document.getElementById("preCalc").value = eval(val);
+  }
+  function ans() {
+      document.getElementById("screen").value = eval(document.getElementById("screen").value);
+  }
+  function reset() {
+    document.getElementById("screen").value = '0';
+  }
+  function back() {
+      let prev = document.getElementById("screen").value;
+      
+      let newVal = document.getElementById("screen").value = prev.slice(0, prev.length-1);
+      if(newVal == "") {
+        result("0");
+        return;
+      }
+      result(newVal);
+  }
