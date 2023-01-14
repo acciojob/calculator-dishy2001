d0dbe92d17dda1e02a18590973e65c14348c7614
Deekshya Karki <99348458+dishy2001@users.noreply.github.com>
2023-01-14 18:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your code here
+function updatescreen(v) {
+  console.log(typeof v);
+  let res = document.getElementById("screen").value += v;
+  if(res.length > 2) {
+    result();
+  }
+}
+
+function result() {
+  let v = document.getElementById("screen").value.at(-1);
+  if(v.match(/\W/g) == null){
+    document.getElementById("screen").value = eval(document.getElementById("screen").value);
+  }
+}
+
+function reset() {
+  document.getElementById("screen").value = '';
+}
